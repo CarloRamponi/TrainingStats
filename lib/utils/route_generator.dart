7131bc7b50e511539d2de81830b97765f239c0a8
Carlo Ramponi <magocarlos1999@gmail.com>
2020-08-13 11:44:44 +0200

    Licensing

--- lib/utils/route_generator.dart
+++ lib/utils/route_generator.dart
@@ -1,10 +1,28 @@
+/*
+ *
+ * Training Stats: mobile app that helps collecting data during
+ * trainings of team sports.
+ * Copyright (C) 2020 Carlo Ramponi, magocarlos1999@gmail.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+ 
+ 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:training_stats/datatypes/action_data.dart';
 import 'package:training_stats/datatypes/exercise_data.dart';
 import 'package:training_stats/datatypes/player_data.dart';
 import 'package:training_stats/datatypes/training_data.dart';
-import 'package:training_stats/routes/about_scene.dart';
 import 'package:training_stats/routes/collect_data_scene.dart';
 import 'package:training_stats/routes/edit_team_scene.dart';
 import 'package:training_stats/routes/players_scene.dart';
@@ -108,16 +126,6 @@ class RouteGenerator {
         return MaterialPageRoute(
             builder: (_) {
               return SettingsScene();
-            }
-        );
-
-        break;
-
-      case "/about":
-
-        return MaterialPageRoute(
-            builder: (_) {
-              return AboutScene();
             }
         );
 
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,8 +1,32 @@
+/*
+ *
+ * Training Stats: mobile app that helps collecting data during
+ * trainings of team sports.
+ * Copyright (C) 2020 Carlo Ramponi, magocarlos1999@gmail.com
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+ 
+ 
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
 
 class MyDrawer extends StatelessWidget {
+
+  final Future<String> licenseFuture = rootBundle.loadString(join("assets", "LICENSE.md"));
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -53,8 +77,26 @@ class MyDrawer extends StatelessWidget {
             leading: Icon(Icons.info),
             title: Text("About"),
             onTap: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).pushReplacementNamed("/about");
+
+              licenseFuture.then((license) {
+
+                String preamble = '''
+Copyright (C) 2020 Carlo Ramponi, magocarlos1999@gmail.com
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it under certain conditions, see the full license below.
+                ''';
+
+                /* Remove useless spaces and returns. */
+                license = license.split("\n").map((e) => e.trim()).map((e) => e == "" ? '\n\n' : e + " ").join("");
+
+                /* Displays a scene where there will be the license of "Training stats" and all other open source licenses of sources that are used in the application */
+                showLicensePage(
+                  context: context,
+                  applicationName: "Training Stats",
+                  applicationVersion: "1.0",
+                  applicationLegalese: preamble + "\n" + license,
+                );
+              });
             },
           ),
         ],
